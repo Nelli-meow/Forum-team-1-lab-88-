cb104d790b8fccbdd59bb5386354ed7b7b27bb79
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2025-01-22 01:23:16 +0600

    F1-5 [api-forum] I created a middleware directory with the auth.ts file. Then the routers directory with the users.ts file to create registration, authorization and exit routes. Then I created the mongoDb.ts file to connect and disconnect the database. And in the index.ts file I created the path to routes and so on.

--- api-forum/index.ts
+++ api-forum/index.ts
@@ -1,13 +1,28 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import mongoDb from "./mongoDb";
+import userRouter from "./routers/users";
 
 const app = express();
 const port =  8000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/users', userRouter);
 
 const run = async () => {
 
+    await mongoose.connect('mongodb://localhost/forum');
+
     app.listen(port, () => {
         console.log(`Server is running at http://localhost:${port}`);
+    });
+
+    process.on('exit', () => {
+        mongoDb.disconnect();
     });
 }
 
