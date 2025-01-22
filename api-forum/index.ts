ddb93840ed785be4eb3ffcaf9557f60bdc776811
nelli gorlova <nelli7035@gmail.com>
2025-01-22 19:50:14 +0600

    F1-9 Connected postsRouter

--- api-forum/index.ts
+++ api-forum/index.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import mongoDb from "./mongoDb";
 import userRouter from "./routers/users";
 import commentsRouter from "./routers/comments";
+import {postRouter} from "./routers/posts";
 
 const app = express();
 const port =  8000;
@@ -14,6 +15,7 @@ app.use(express.static('public'));
 
 app.use('/users', userRouter);
 app.use('/comments', commentsRouter);
+app.use('/posts', postRouter);
 
 const run = async () => {
 
