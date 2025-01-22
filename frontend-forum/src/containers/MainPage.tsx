dd6f206e3aedcf20ecbe9cddbaf4ffab306fdeea
nelli gorlova <nelli7035@gmail.com>
2025-01-23 02:13:33 +0600

    F1-13 Corrected the text

--- frontend-forum/src/containers/MainPage.tsx
+++ frontend-forum/src/containers/MainPage.tsx
@@ -1,6 +1,5 @@
-
 import { useAppSelector } from '../app/hooks.ts';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { selectUser } from '../features/users/UsersSlice.ts';
 import Header from '../components/Header/Header.tsx';
 
@@ -12,7 +11,18 @@ const MainPage = () => {
       <Header/>
       <Box sx={{ flexGrow: 1 }}>
         {
-          user ? <p>ЕСЛИ ЮЗЕР ЕСТЬ ТО СЮДА УЖЕ КОМПОНЕНТ С ПОСТАМИ ЙОУ ДА</p> :<p className="text-center my-5">Login or create an account :)</p>
+          user ? <p>ЕСЛИ ЮЗЕР ЕСТЬ ТО СЮДА УЖЕ КОМПОНЕНТ С ПОСТАМИ ЙОУ ДА</p> :<Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Typography variant="body1">
+              Login or create an account :)
+            </Typography>
+          </Box>
         }
       </Box>
     </>
