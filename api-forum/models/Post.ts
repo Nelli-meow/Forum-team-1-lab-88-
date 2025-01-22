c436c526b9d1fc8e2ea48f566ecb4cc08b7e44bd
Sultan Mukhtarov <suli419956@gmail.com>
2025-01-22 11:22:30 +0600

    Created a post model. After creating a model of comments, need to add key of comment in Post model.

---
+++ api-forum/models/Post.ts
@@ -0,0 +1,32 @@
+import mongoose, {Types} from 'mongoose';
+import User from "./User";
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+
+        validate: {
+            validator: async (value: Types.ObjectId) => {
+                const user = await User.findById(value);
+                return Boolean(user);
+            },
+            message: 'User does not exist!',
+        },
+    },
+    image: String,
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export const Post = mongoose.model('Post', PostSchema);
